420714fcde5fde8dc5dc69ed412204899c8579fa
bjryan2 <bjryan2@illinois.edu>
2014-12-27 19:53:41 -0500

    theme

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+ /* globals jQuery, document */
+ (function ($, undefined) {
+  "use strict";
+
+  var $document = $(document);
+
+  // hacky fix to make images work with pure grid
+  $('img').addClass('pure-img centered');
+
+})(jQuery);
